abf8da3b8f8fc16527aa7cd84e1c0feed68871b9
SrgArt <xcxserj@mail.ru>
2023-06-15 18:55:16 +0300

    tests = 100% cov

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -10,3 +10,8 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1) == []
+    assert arrs.my_slice([1, 2, 3, 4], -2) == [3, 4]
+    assert arrs.my_slice([1, 2, 3, 4], -3, -1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], 0) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3], -4) == [1, 2, 3]
